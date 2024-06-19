6be53bcea54f94a32378881043a5a489fbbaa425
ml529 <145190959+ml529@users.noreply.github.com>
2024-06-19 21:01:58 +0800

    Update baidu.py

--- 2024_training/第三周/baidu.py
+++ 2024_training/第三周/baidu.py
@@ -1,6 +1,5 @@
 import threading 
 import requests
-
 #下载函数 def download_html(url, thread_id): try: response = requests.get(url) if response.status_code == 200: print(f"Thread {thread_id}: Downloaded {len(response.text)} characters from {url}") else: print(f"Thread {thread_id}: Failed to download {url}, status code {response.status_code}") except Exception as e: print(f"Thread {thread_id}: Error occurred: {e}")
 
 #要下载的URL url = 'http://www.baidu.com'
